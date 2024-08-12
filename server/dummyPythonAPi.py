518ffd06be36bdb8f95b21c971863c34d0ccfd69
DukeTriggah <ntoutoume36@gmail.com>
2024-08-12 16:50:29 -0500

    Added functions to server.js and dummyPythonAPi.py that fetches weather data from the api. Added dictionary to server,js that has lists of all the F1 teams, their drivers, and thier numbers

--- server/dummyPythonAPi.py
+++ server/dummyPythonAPi.py
@@ -15,9 +15,27 @@ def process_data(data):
     # Example: Print the data
     print(data)
 
+# Method to fetch car data from a particular race from specified racer
+def get_race_info():
+    print("yo")
+
+
+# Method to fetch weather inforation from a particular race activity
+def get_weather_data():
+    try:
+        response = requests.get('http://localhost:3000/weather-data')  # Ensure the server is running
+        response.raise_for_status()
+        data = response.json()
+        return data
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching weather data: {e}")
+        return None
+
 if __name__ == "__main__":
-    api_data = fetch_api_data()
+    api_data = get_weather_data()
     if api_data:
         process_data(api_data)
 
-# python script.py
+
+
+
